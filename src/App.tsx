c8e12dabea83217b127d176c2d6581241e3950cc
GadhiyaVatsal <gadhiya.vatsal007@gmail.com>
2024-01-11 15:44:32 -0500

    Implemented the button to roll dice

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,76 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   Image,
   ImageSourcePropType,
+  Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
-import DiceOne from '../assets/One.png'
-import DiceTwo from '../assets/Two.png'
-import DiceThree from '../assets/Three.png'
-import DiceFour from '../assets/Four.png'
-import DiceFive from '../assets/Five.png'
-import DiceSix from '../assets/Six.png'
+import DiceOne from '../assets/One.png';
+import DiceTwo from '../assets/Two.png';
+import DiceThree from '../assets/Three.png';
+import DiceFour from '../assets/Four.png';
+import DiceFive from '../assets/Five.png';
+import DiceSix from '../assets/Six.png';
 
 type DiceProps = PropsWithChildren<{
-  imageUrl: ImageSourcePropType
-}>
+  imageUrl: ImageSourcePropType;
+}>;
 
-const Dice = ({ imageUrl }: DiceProps): JSX.Element => { 
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
   return (
     <View>
       <Image style={styles.diceImage} source={imageUrl} />
     </View>
-  )
-}
+  );
+};
 
-function App(): React.JSX.Element { 
+function App(): React.JSX.Element {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollDiceOnTap = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+      default:
+        setDiceImage(DiceOne);
+        break;
+    }
+  }
+
   return (
-    <SafeAreaView>
-    <View>
-      <Text>Test text</Text>
-    </View>
-    </SafeAreaView>
-  )
+      <View style={styles.container}>
+        <Dice imageUrl={diceImage} />
+        <Pressable onPress={rollDiceOnTap}>
+          <Text style={styles.rollDiceBtnText}>Roll the dice</Text>
+        </Pressable>
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
